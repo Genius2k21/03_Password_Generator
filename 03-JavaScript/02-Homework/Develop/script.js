45d22c5737a3e8236b5112876c721250b2616d9c
JBlake2k21 <69338885+JBlake2k21@users.noreply.github.com>
2021-07-15 21:52:35 -0400

    generate password function

--- 03-JavaScript/02-Homework/Develop/script.js
+++ 03-JavaScript/02-Homework/Develop/script.js
@@ -1,6 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//function to generate password
+function generatePassword(){
+  //Creating the password placeholder
+  var pass = '';
+  //Generating the password string using random function
+  for (i=1; i<=passLen; i++){
+    var Char = Math.floor(Math.random()*charactersSet.length + 1);
+      //updating the password value with the generated random string
+      pass +=charactersSet.charAT(Char);
+  }
+  return pass; 
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
